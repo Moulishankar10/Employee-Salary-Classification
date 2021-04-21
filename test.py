55e9af2d3423de43ed8e011e9525d5c2af779eeb
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-21 20:04:07 +0530

    Created the testing file

---
+++ test.py
@@ -0,0 +1,13 @@
+# EMPLOYEE SALARY CLASSIFICATION
+
+# DEVELOPED BY
+# MOULISHANKAR M R 
+
+#IMPORTING MODULES
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from tensorflow.keras.models import save_model 
+import matplotlib.pyplot as plt
