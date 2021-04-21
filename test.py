040082e8a09ac1120d4cb4409410c469cc88dc20
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-21 21:13:50 +0530

    Modified the code

--- test.py
+++ test.py
@@ -10,3 +10,15 @@ from tensorflow.keras.models import load_model
 
 # LOADING THE TRAINED MODEL
 model = load_model("model/model",custom_objects=None,compile=True)
+
+# INPUT DATA
+print("\n\n Enter the required details of an employee to explore the employee's salary range. \n\n")
+
+sl = int(input("\nSatisfaction level of the Employee's performance (%) : "))
+le = int(input("\nRecent Evaluation of the Employee (%) : "))
+nop = int(input("\nNumber of projects completed successfully : "))
+amh = int(input("\nAverage working hours per month : "))
+yac = int(input("\nNumber of Years working in the company : "))
+
+# PROCESSING INPUT DATA
+details = [sl,le,nop,amh,yac]
