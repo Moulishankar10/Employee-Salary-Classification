88ff892a49892f4d6b058eabfae682d488bbf923
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-21 21:15:42 +0530

    Updated the testing file

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import pandas as pd
 from tensorflow.keras.models import load_model
 
 # LOADING THE TRAINED MODEL
-model = load_model("model/model",custom_objects=None,compile=True)
+model_test = load_model("model/model",custom_objects=None,compile=True)
 
 # INPUT DATA
 print("\n\n Enter the required details of an employee to explore the employee's salary range. \n\n")
@@ -22,3 +22,13 @@ yac = int(input("\nNumber of Years working in the company : "))
 
 # PROCESSING INPUT DATA
 details = [sl,le,nop,amh,yac]
+
+# DEFINING CLASSIFICATIONS
+sal_range = ['Low', 'Medium', 'High']
+
+# CLASSIFYING THE USER DATA
+res = model_test.predict([details])
+res = np.argmax(res,axis = 1)
+
+# DISPLAYING THE RESULT
+print(f"\n\n Thus, the Employee's Salary Range might be categorized under {sal_range[int(res)]} category!")
